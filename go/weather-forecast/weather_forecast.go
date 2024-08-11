41d5acd73d33fe253815883e082660e0598d83f5
Chinomnso Chinedum <chinomnsoc@gmail.com>
2024-08-11 21:04:19 +0100

    added comments for variables, functions and packages

--- go/weather-forecast/weather_forecast.go
+++ go/weather-forecast/weather_forecast.go
@@ -1,8 +1,12 @@
+// Package weather provides current weather information for a specified city.
 package weather
 
+// CurrentCondition is the current weather condition.
 var CurrentCondition string
+// CurrentLocation is the current location for which the weather is being described.
 var CurrentLocation string
 
+// Forecast returns the current weather conditions for a specified city.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
