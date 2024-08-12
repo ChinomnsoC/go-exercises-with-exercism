e2f1f02af49c42cc980b6d15bc474fb6bfb7429a
Chinomnso Chinedum <chinomnsoc@gmail.com>
2024-08-12 23:57:47 +0100

    Working with strings in golang

--- go/welcome-to-tech-palace/welcome_to_tech_palace.go
+++ go/welcome-to-tech-palace/welcome_to_tech_palace.go
@@ -1,16 +1,21 @@
 package techpalace
 
+import "strings"
+
 // WelcomeMessage returns a welcome message for the customer.
 func WelcomeMessage(customer string) string {
-	panic("Please implement the WelcomeMessage() function")
+	return "Welcome to the Tech Palace, " + strings.ToUpper(customer)
 }
 
 // AddBorder adds a border to a welcome message.
 func AddBorder(welcomeMsg string, numStarsPerLine int) string {
-	panic("Please implement the AddBorder() function")
+	var starString string = strings.Repeat("*", numStarsPerLine)
+	return starString+"\n"+welcomeMsg+"\n"+starString
 }
 
 // CleanupMessage cleans up an old marketing message.
 func CleanupMessage(oldMsg string) string {
-	panic("Please implement the CleanupMessage() function")
+	oldMsg = strings.ReplaceAll(oldMsg, "*", "")
+	oldMsg = strings.TrimSpace(oldMsg)
+	return oldMsg
 }
