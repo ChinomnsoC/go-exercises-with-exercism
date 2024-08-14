8caa87f45cd596362d7ba2db5a2dfee73175bcfe
Chinomnso Chinedum <chinomnsoc@gmail.com>
2024-08-14 15:46:55 +0100

    Implementing Sprintf() in golang

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -1,15 +1,17 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package twofer should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package twofer determines what to say in a cookie offer statement from a bakery
 package twofer
 
-// ShareWith should have a comment documenting it.
+import (
+	"fmt"
+)
+
+// ShareWith offers two cookies for the price one to your specified name or to a generic you.
 func ShareWith(name string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+	var offer string = "One for you, one for me."
+	if name != "" {
+		return fmt.Sprintf("One for %s, one for me.", name)
+	} else {
+		return offer
+	}
+	
 }
