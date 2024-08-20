420e6392a93d45efb85d6c5d20b6254844d02ca8
Chinomnso Chinedum <chinomnsoc@gmail.com>
2024-08-20 22:04:07 +0100

    adding helper function

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -3,3 +3,15 @@ package raindrops
 func Convert(number int) string {
 	panic("Please implement the Convert function")
 }
+
+func isDivisibleBy3(number int) bool {
+	return number%3 == 0
+}
+
+func isDivisibleBy5(number int) bool {
+	return number%5 == 0
+}
+
+func isDivisibleBy7(number int) bool {
+	return number%7 == 0
+}
